6e2892be5e00c891cd0f76df696ce69e92f1950c
kubilayciftci <ciftcikubi@gmail.com>
2025-07-23 18:59:07 +0300

    initial commit

---
+++ src/main/java/com/kciftci/inghubsplatform/INGHubsPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.kciftci.inghubsplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class INGHubsPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(INGHubsPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kciftci/inghubsplatform/IngHubsPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kciftci.inghubsplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngHubsPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
